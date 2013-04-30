ac9084e00f16f53a8245c9cdfdfbc9dfaaf58364
Leonardo <Hernandez>
2013-04-30 11:06:13 -0500

    newstring
    connectionstring for azure

--- conexion/Conect_DB.php
+++ conexion/Conect_DB.php
@@ -1,9 +1,9 @@
 <?php
 		
-	$hostname_cn_MySQL = "localhost";
-	$database_cn_MySQL = "tqgroup_awid";
-	$username_cn_MySQL = "tqgroup_leonardo";
-	$password_cn_MySQL = "T&QGroup";
+	$hostname_cn_MySQL = "us-cdbr-azure-northcentral-a.cleardb.com";
+	$database_cn_MySQL = "sapweb";
+	$username_cn_MySQL = "b6b6cbe16f18d7";
+	$password_cn_MySQL = "a5b3c63f";
 
 	
 
